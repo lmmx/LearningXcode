4d41b22b126eb129544345deaeb23594eefd085d
Louis Maddox <louismmx@gmail.com>
2021-06-15 11:39:09 +0100

    Initial commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Louis M on 15/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Louis?")
+            .padding()
+    }
+}
---
+++ Shared/LearningXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningXcodeApp.swift
+//  Shared
+//
+//  Created by Louis M on 15/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
